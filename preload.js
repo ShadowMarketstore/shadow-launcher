c4e333698136d0b50ac2d10d27c3fafe7d019d05
ShadowMarketstore <support.shadowmarket@gmail.com>
2026-07-28 17:44:43 +0000

    Update preload.js

--- preload.js
+++ preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('shadowAPI', {
   scanProducts: () => ipcRenderer.invoke('scan-products'),
   executeScripts: (files) => ipcRenderer.invoke('execute-scripts', files),
-  restartPC: () => ipcRenderer.invoke('restart-pc')
+  restartPC: () => ipcRenderer.invoke('restart-pc'),
+  openScriptsFolder: () => ipcRenderer.invoke('open-scripts-folder')
 });
